b92440883e36e6e863c2adc721e3ddbe78086529
atarayas <32460358+atarayas@users.noreply.github.com>
2019-03-13 10:41:38 -0600

    cambios de prueba react

---
+++ js/default-babel.js
@@ -0,0 +1,8 @@
+class Pie extends React.Component {
+              render() {
+                  return (
+                      <span>Copyright &copy; Ministerio de Educación Pública de Costa Rica 2019 </span>
+                  );
+              }
+          };
+ReactDOM.render(<Pie />, document.getElementById('text_footer'));
